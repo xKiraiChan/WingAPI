509a390347c35b04422b490b5eca0eb16e918b33
xKiraiChan <xKiraiChan@gmail.com>
2021-10-19 21:27:59 -0400

    add nested pages

--- Extensions.cs
+++ Extensions.cs
@@ -6,6 +6,7 @@ namespace KiraiMod.WingAPI
     public static class Extensions
     {
         public static WingPage CreatePage(this Wing.BaseWing page, string name) => new WingPage(page, name);
+        public static WingPage CreateNestedPage(this WingPage page, string name, int index) => new WingPage(page, name, index);
         public static WingButton CreateButton(this WingPage page, string name, int index, System.Action onClick) => new WingButton(page, name, index, onClick);
         public static WingToggle CreateToggle(this WingPage page, string name, int index, Color on, Color off, bool initial, System.Action<bool> onClick) => new WingToggle(page, name, index, on, off, initial, onClick);
     }
--- RawUI/WingPage.cs
+++ RawUI/WingPage.cs
@@ -1,10 +1,13 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace KiraiMod.WingAPI.RawUI
 {
     public class WingPage
     {
+        public static List<WingPage> openedPages = new List<WingPage>();
+
         public Wing.BaseWing wing;
         public Transform transform;
         public TMPro.TextMeshProUGUI text;
@@ -28,7 +31,13 @@ namespace KiraiMod.WingAPI.RawUI
             closeButton.onClick.AddListener(new System.Action(() =>
             {
                 transform.gameObject.SetActive(false);
-                wing.WingMenu.gameObject.SetActive(true);
+                openedPages.RemoveAt(openedPages.Count - 1);
+                if (openedPages.Count > 0)
+                {
+                    WingPage prev = openedPages[openedPages.Count - 1];
+                    prev.transform.gameObject.SetActive(true);
+                }
+                else wing.WingMenu.gameObject.SetActive(true);
             }));
 
             Transform open = Object.Instantiate(wing.ProfileButton, wing.WingButtons);
@@ -38,7 +47,58 @@ namespace KiraiMod.WingAPI.RawUI
             openButton.onClick = new Button.ButtonClickedEvent();
             openButton.onClick.AddListener(new System.Action(() => {
                 transform.gameObject.SetActive(true);
-                wing.WingMenu.gameObject.SetActive(false);
+                openedPages.Add(this);
+                if (openedPages.Count > 1)
+                {
+                    WingPage prev = openedPages[openedPages.Count - 2];
+                    prev.transform.gameObject.SetActive(false);
+                }
+                else wing.WingMenu.gameObject.SetActive(false);
+            }));
+        }
+
+        public WingPage(WingPage page, string name, int index)
+        {
+            wing = page.wing;
+
+            transform = Object.Instantiate(wing.ProfilePage, wing.WingPages);
+            Transform content = transform.Find("ScrollRect/Viewport/VerticalLayoutGroup");
+            transform.gameObject.SetActive(false);
+
+            for (int i = 0; i < content.GetChildCount(); i++)
+                Object.Destroy(content.GetChild(i).gameObject);
+            transform.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = name;
+
+            closeButton = transform.GetComponentInChildren<Button>();
+            closeButton.onClick = new Button.ButtonClickedEvent();
+            closeButton.onClick.AddListener(new System.Action(() =>
+            {
+                transform.gameObject.SetActive(false);
+                openedPages.RemoveAt(openedPages.Count - 1);
+                if (openedPages.Count > 0)
+                {
+                    WingPage prev = openedPages[openedPages.Count - 1];
+                    prev.transform.gameObject.SetActive(true);
+                } else wing.WingMenu.gameObject.SetActive(true);
+            }));
+
+            Transform open = Object.Instantiate(wing.ProfileButton, page.transform);
+            (text = open.GetComponentInChildren<TMPro.TextMeshProUGUI>()).text = name;
+
+            openButton = open.GetComponent<Button>();
+            openButton.GetComponent<RectTransform>().sizeDelta = new Vector2(420, 144);
+            openButton.transform.localPosition = new Vector3(0, 320 - (index * 120), transform.transform.localPosition.z);
+            openButton.onClick = new Button.ButtonClickedEvent();
+            openButton.onClick.AddListener(new System.Action(() =>
+            {
+                transform.gameObject.SetActive(true);
+                openedPages.Add(this);
+                if (openedPages.Count > 1)
+                {
+                    WingPage prev = openedPages[openedPages.Count - 2];
+                    prev.transform.gameObject.SetActive(false);
+                }
+                else wing.WingMenu.gameObject.SetActive(false);
             }));
         }
     }
